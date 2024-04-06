f717e6ce632ef72caedc5bcb2d800142c4a50f78
Quim Moran M <marc.moran@estudiantat.upc.edu>
2024-04-06 12:13:08 +0200

    fix: Deleting all the files that are not involved in this BackOffice.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { NavigationComponent } from './components/navigation/navigation.componen
 import { AppComponent } from './components/app/app.component';
 import { HomeComponent } from './components/home/home.component';
 import { LocationComponent } from './components/location-components/location/location.component';
-import { CommentComponent } from './components/comment-components/comment/comment.component';
 import { UserDetailsComponent } from './components/user-components/user-details/user-details.component';
 import { UserEditComponent } from './components/user-components/user-edit/user-edit.component';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
@@ -17,26 +16,9 @@ import { LocationCreateComponent } from './components/location-components/locati
 import { LocationDetailsComponent } from './components/location-components/location-details/location-details.component';
 import { LocationEditComponent } from './components/location-components/location-edit/location-edit.component';
 import { LogInComponent } from './components/log-in/log-in.component';
-import { CommentEditComponent } from './components/comment-components/comment-edit/comment-edit.component';
-import { CommentDetailsComponent } from './components/comment-components/comment-details/comment-details.component';
-import { CommentCreateComponent } from './components/comment-components/comment-create/comment-create.component';
 import { RegisterComponent } from './components/register/register.component';
-import { PublicationComponent } from './components/publication-components/publication/publication.component';
-import { PublicationCreateComponent } from './components/publication-components/publication-create/publication-create.component';
-import { PublicationDetailsComponent } from './components/publication-components/publication-details/publication-details.component';
-import { PublicationEditComponent } from './components/publication-components/publication-edit/publication-edit.component';
-import { CommentResponseComponent } from './components/comment-components/comment-response/comment-response.component';
-import { ApplicationComponent } from './components/application-components/application/application.component';
-import { ApplicationCreateComponent } from './components/application-components/application-create/application-create.component';
-import { ApplicationDetailsComponent } from './components/application-components/application-details/application-details.component';
-import { ApplicationEditComponent } from './components/application-components/application-edit/application-edit.component';
-import { ActivityComponent } from './components/activity-components/activity/activity.component';
-import { ActivityCreateComponent } from './components/activity-components/activity-create/activity-create.component';
-import { ActivityDetailsComponent } from './components/activity-components/activity-details/activity-details.component';
-import { ActivityEditComponent } from './components/activity-components/activity-edit/activity-edit.component';
 import { AuthGuard } from './shared/auth.guard';
 import { AuthService } from './services/auth.service';
-import { ActivityParticipantsComponent } from './components/activity-components/activity-participants/activity-participants.component';
 import { NotificationModalComponent } from './components/notification-modal/notification-modal.component';
 
 const routes: Routes = [
@@ -101,11 +83,6 @@ const routes: Routes = [
     NavigationComponent,
     AppComponent,
     HomeComponent,
-    CommentComponent,
-    CommentCreateComponent,
-    CommentDetailsComponent,
-    CommentEditComponent,
-    CommentResponseComponent,
     LogInComponent,
     RegisterComponent,
     UserDetailsComponent,
@@ -117,19 +94,6 @@ const routes: Routes = [
     LocationCreateComponent,
     LocationDetailsComponent,
     LocationEditComponent,
-    PublicationComponent,
-    PublicationCreateComponent,
-    PublicationDetailsComponent,
-    PublicationEditComponent,
-    ApplicationComponent,
-    ApplicationCreateComponent,
-    ApplicationDetailsComponent,
-    ApplicationEditComponent,
-    ActivityComponent,
-    ActivityCreateComponent,
-    ActivityDetailsComponent,
-    ActivityEditComponent,
-    ActivityParticipantsComponent,
 
     // NEW: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -148,4 +112,5 @@ const routes: Routes = [
   providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ActivityService } from 'src/app/services/activity.service';
-import { ApplicationService } from 'src/app/services/application.service';
-import { CommentService } from 'src/app/services/comment.service';
-import { LocationService } from 'src/app/services/location.service';
-import { PublicationService } from 'src/app/services/publication.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -21,28 +16,30 @@ export class HomeComponent {
   publicationCount:string="0";
   userCount:string="0";
 
-  constructor(private route: ActivatedRoute, private publicationService: PublicationService,private applicationService:ApplicationService,private activityService:ActivityService,private commentService:CommentService,private locationService:LocationService,private userService:UserService,private router:Router) {}
+  constructor(private route: ActivatedRoute,private userService:UserService,private router:Router) {}
 
   ngOnInit(){
-    this.locationService.getCountLocation().subscribe((locations) => {
-      this.locationCount=locations;
-    });
-    this.publicationService.getCountPublication().subscribe((publications)=>{
-      this.publicationCount=publications;
-    });
-    this.applicationService.getCountApplication().subscribe((applications)=>{
-      this.applicationCount=applications;
-    });
-    this.activityService.getCountActivity().subscribe((activities)=>{
-      this.activityCount=activities;
-    });
+    // Users (Number): 
     this.userService.getCountUser().subscribe((users)=>{
       this.userCount=users;
     });
-    this.commentService.getCountComment().subscribe((comments)=>{
-      this.commentCount=comments;
-    });
-    
+    // Cards (Number): 
+    // Prizes (Number):   
+    // Services (Number): 
+    // Locations (Number): 
+    // Shops (Number): 
+    // Incidents (Number): 
+    // Offers (Number): 
+    // Products (Number): 
+    // News (Number): 
+    // Preferences (Number): 
+    // Predictions (Number): 
+    // Flights (Number): 
+    // Luggage (Number): 
+    // Matches (Number): 
+    // Bookings (Number): 
+    // Games (Number): 
+    // Questions (Number): 
   }
 
 }
